8e24663dbc86fb954b91a8bb5354da0abd1a6ff8
Dhruv Mangukiya <159777363+dhurv99137@users.noreply.github.com>
2024-05-16 18:04:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 
-app.get('/', (req, res) => {
-    res.send('get data in server');
-});
-
-
-let arr = []; 
+let app = express();
 
 app.use(express.json());
-app.post('/', (req, res) => {
-    arr.push(req.body); 
-    res.send('post method');
+let databas = [];
+
+app.get("/", (req, res) => {
+    let { category, sort, search } = req.query
+    let temp = databas
+
+    if (search) {
+        temp = temp.filter((ele) => ele.title == search)
+    }
+
+    if (category) {
+        temp = databas.filter((ele) => ele.category == category)
+    }
+
+    if (sort == "lth") {
+        temp.sort((a, b) => a.price - b.price)
+    }
+    else {
+        temp.sort((a, b) => b.price - a.price)
+    }
+
+    res.send(temp)
+})
+
+// get method
+app.get("/", (req, res) => {
+    let { id } = req.params
+    res.send(databas[id]);
 });
 
-app.listen(3000, () => {
-    console.log("Server is running");
+// post method
+app.post("/", (req, res) => {
+    databas.push(req.body);
+
+    res.send(databas);
 });
 
+// delete method
+app.delete("/:id", (req, res) => {
+    let { id } = req.params;
+    databas.splice(id, 1);
+
+    res.send(databas);
+});
+
+// patch method
+app.patch("/:id", (req, res) => {
+    let { id } = req.params;
+    databas[id] = { ...databas[id], ...req.body };
+
+    res.send(databas);
+});
+
+app.listen(8090, () => {
+    console.log("server running up to 8090");
+});
